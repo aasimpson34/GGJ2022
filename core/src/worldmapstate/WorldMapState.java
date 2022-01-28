e9a7fb7e98cc7ba66a39c2126182eb53e78df8c9
Aaron Simpson <aasimpson34@googlemail.com>
2022-01-28 22:13:06 +0000

    #3 Added initial Game Object Handling into the World Map State

--- core/src/worldmapstate/WorldMapState.java
+++ core/src/worldmapstate/WorldMapState.java
@@ -1,17 +1,26 @@
 package worldmapstate;
 
+import entity.GameObjectEntityHandler;
 import states.GameState;
 
 public class WorldMapState implements GameState {
-
+	
+	
+	public WorldMapState() {
+		
+	}
+	
 	@Override
 	public GameState update() {
 
+		GameObjectEntityHandler.getInstance().update();
+		
 		return this;
 	}
 
 	@Override
 	public void render() {
+		GameObjectEntityHandler.getInstance().render();
 
 	}
 
