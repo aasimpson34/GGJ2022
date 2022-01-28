5c7a050a741f45b93a013b69da0656da868f2803
Aaron Simpson <aasimpson34@googlemail.com>
2022-01-28 23:05:11 +0000

    #5 Initial Chunk Generation and World Chunk class

--- core/src/worldgeneration/WorldGenerator.java
+++ core/src/worldgeneration/WorldGenerator.java
@@ -1,5 +1,7 @@
 package worldgeneration;
 
+import java.util.Random;
+
 public class WorldGenerator {
 	
 	/**
@@ -10,7 +12,17 @@ public class WorldGenerator {
 	 */
 	public void generateChunk(int x, int y)
 	{
+		//Unique seed given by : 1/2(a+b)(a+b+1)+b
+		long unique_seed_number = (long)(0.5F  * (x + y) * (x + y + 1) + y);
 		
+		Random random_generator = new Random(unique_seed_number);
+		
+		//Randomly select the number of towns that will be assigned
+		int numberOfTownsToGenerate = random_generator.nextInt(10);
+		
+		//Decide on town positions
+		
+		//Generate towns
 	}
 	
 	/**
---
+++ core/src/worldmap/WorldChunk.java
@@ -0,0 +1,11 @@
+package worldmap;
+
+public class WorldChunk {
+	int m_xChunkPosition;
+	int m_yChunkPosition;
+	
+	public WorldChunk(int x, int y)
+	{
+		
+	}
+}
