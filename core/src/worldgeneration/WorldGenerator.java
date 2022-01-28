a303607501a0ef7fbb880404d681969fadfa1188
Aaron Simpson <aasimpson34@googlemail.com>
2022-01-28 21:14:58 +0000

    #2 Initial world generator framework.

---
+++ core/src/worldgeneration/WorldGenerator.java
@@ -0,0 +1,26 @@
+package worldgeneration;
+
+public class WorldGenerator {
+	
+	/**
+	 * This operation will generate a world chunk
+	 * (where a chunk is a 32x32 tiled area.
+	 * @param x
+	 * @param y
+	 */
+	public void generateChunk(int x, int y)
+	{
+		
+	}
+	
+	/**
+	 * Call this operation to generate a town 
+	 * @param x tile position
+	 * @param y tile position
+	 */
+	public void generateTown(int x, int y)
+	{
+		
+	}
+
+}
