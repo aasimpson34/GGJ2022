0c88d9acaa7f346627e8f266cd107b3cb14c1878
Aaron Simpson <aasimpson34@googlemail.com>
2022-01-28 21:46:41 +0000

    #13 Add X and Y to generate operation.

--- core/src/towngeneration/TownGeneratorHandler.java
+++ core/src/towngeneration/TownGeneratorHandler.java
@@ -4,7 +4,7 @@ import townentity.TownEntity;
 
 public class TownGeneratorHandler {
 	
-	public TownEntity generateNewTown()
+	public TownEntity generateNewTown(int x, int y)
 	{
 		System.err.println("TownGeneratorHandler: Not implemented");
 		return null;
