e25beba90354c439cacc50bc122d689d85ddd9f1
Cullen Willis <cullenwillis96@gmail.com>
2022-01-29 22:13:15 +0000

    #17 TownWorldEntity base UI

--- core/src/main/java/userInterface/TownWorldEntityUI.java
+++ core/src/main/java/userInterface/TownWorldEntityUI.java
@@ -8,7 +8,10 @@
  */
 package userInterface;
 
+import com.badlogic.gdx.graphics.Color;
+
 import townentity.TownEntity;
+import townentity.TownResources;
 
 /**
  * @author Cullen Willis
@@ -20,12 +23,44 @@ public class TownWorldEntityUI extends WorldEntityUI {
 	@Override
 	public void update() {
 		townEntity = getTownEntity();
-		
 	}
 
 	@Override
 	public void render() {
-		renderWindow(0, 0, 200, 200);
+		renderWindow(0, 0, 800, 600, 0);
+		renderWindow(800, 100, 200, 400, 0);
+		renderButton("Engage Battle", 25, 25, 750, 50);
+		
+		if(this.townEntity == null) {
+			return;
+		}
+		
+		String populationText = "Population: " + this.townEntity.getPopulation();
+		renderLabel(populationText, 825, 475, 150, Color.RED, 1, -1);
+		
+		String TownNameText = "Temp Name";
+		renderLabel(TownNameText, 25, 560, 750, Color.RED, 3, 1);
+		
+		renderWindow(25, 500, 750, 10, 1);
+		renderLabel("Hated", 25, 475, 100, Color.WHITE, 1, -1);
+		renderLabel("Neutral", 25, 475, 750, Color.WHITE, 1, 1);
+		renderLabel("Friendly", 675, 475, 100, Color.WHITE, 1, 0);
+		
+		int barPositionX = mapValueToRange(this.townEntity.getReputation(), 0, 100, 25, 775);
+		renderWindow(barPositionX, 498, 5, 14, 2);
+		
+		TownResources[] resources = this.townEntity.getAllResources();
+		for(int i = 0; i < resources.length; i++) {
+			TownResources el = resources[i];
+			String resourceText = el.getResourceType().toString() + ": " + el.getAmount() + "/" + el.getResourceLimit();
+			renderLabel(resourceText, 825, 450 - (i * 25), 150, Color.RED, 1, -1);
+		}
+		
+		
+	}
+	
+	private int mapValueToRange(int value, int currentMin, int currentMax, int targetMin, int targetMax) {
+		return targetMin + (targetMax - targetMin) * (value - currentMin) / (currentMax - currentMin);
 	}
 
 }
