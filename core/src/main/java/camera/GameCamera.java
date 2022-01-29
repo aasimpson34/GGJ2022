81875cb08339068b8c06a9a7ebcecee357462be6
Aaron Simpson <aasimpson34@googlemail.com>
2022-01-29 10:11:27 +0000

    #15 Initial camera implementation
    Co-Authored-By: Cullen Willis <cullenwillis96@gmail.com>

---
+++ core/src/main/java/camera/GameCamera.java
@@ -0,0 +1,79 @@
+/**
+ * Project Name: GGJ2022-core 
+ * Created On: 29 Jan 2022
+ * file: GameCamera.java
+ * Purpose of class: 
+ *
+ * Written by @author Aaron Simpson
+ */
+package camera;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * @author aasim
+ *
+ */
+public class GameCamera {
+	static GameCamera m_instance;
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static GameCamera getInstance()
+	{
+		if(m_instance == null)
+			m_instance = new GameCamera();
+		
+		return m_instance;
+	}
+	
+	Vector2 m_targetPosition;
+	float m_speedToMoveToPosition;
+	
+	OrthographicCamera m_gameCameraMatrix;
+	
+	public GameCamera()
+	{
+		m_gameCameraMatrix = new OrthographicCamera(1920,1080);
+		
+	}
+	
+	/**
+	 * Update method called via the state handlers
+	 */
+	public void update()
+	{
+		//Interpolation formula 
+		float interpolate_amount = (float) (1.0 - Math.pow(1 - m_speedToMoveToPosition * 3.0f, Gdx.graphics.getDeltaTime()* 60));
+		m_gameCameraMatrix.position.x  = Interpolation.linear.apply(m_gameCameraMatrix.position.x, m_targetPosition.x, interpolate_amount);
+		m_gameCameraMatrix.position.y  = Interpolation.linear.apply(m_gameCameraMatrix.position.y, m_targetPosition.y, interpolate_amount);
+		
+		m_gameCameraMatrix.update();
+	}
+	
+	/**
+	 * This operation is a handy function that will allow the user to 
+	 * move to a certain area over a specified time period
+	 * 
+	 * if <time> == 0 then it will move instantly.
+	 * @param x
+	 * @param y
+	 * @param time
+	 */
+	public void moveTo(float x, float y, float speed)
+	{
+		m_targetPosition = new Vector2(x, y);
+		m_speedToMoveToPosition = speed;
+	}
+	
+	public Vector3 getMouseCoords(Vector2 mousePosition)
+	{
+		return m_gameCameraMatrix.unproject(new Vector3(mousePosition, 0));
+	}
+}
