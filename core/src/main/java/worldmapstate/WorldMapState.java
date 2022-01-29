e7aeff6b1baff7fcc01d373e5f2935fd85b3f68c
Aaron Simpson <aasimpson34@googlemail.com>
2022-01-29 10:25:33 +0000

    #16 plug the asset management system into the world map state
    Co-Authored-By: Cullen Willis <cullenwillis96@gmail.com>

--- core/src/main/java/worldmapstate/WorldMapState.java
+++ core/src/main/java/worldmapstate/WorldMapState.java
@@ -1,27 +1,38 @@
 package worldmapstate;
 
+import assetmanager.LoadingScreen;
+import camera.GameCamera;
 import entity.GameObjectEntityHandler;
+import renderer.GameRenderer;
 import states.GameState;
 
 public class WorldMapState implements GameState {
 	
+	LoadingScreen loadingScreen;
 	
 	public WorldMapState() {
-		
+		GameRenderer.getInstance();
+		GameCamera.getInstance();
+		loadingScreen = new LoadingScreen();
 	}
 	
 	@Override
 	public GameState update() {
 
 		GameObjectEntityHandler.getInstance().update();
-		
+		loadingScreen.update();
 		return this;
 	}
 
 	@Override
 	public void render() {
+		GameRenderer.getInstance().getBatch().setProjectionMatrix(GameCamera.getInstance().getProjection());
+		GameRenderer.getInstance().getBatch().begin();
+		
 		GameObjectEntityHandler.getInstance().render();
-
+		loadingScreen.render();
+		
+		GameRenderer.getInstance().getBatch().end();
 	}
 
 	@Override
