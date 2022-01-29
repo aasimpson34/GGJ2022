f9138c910081ba572dde1d1dc0a8b6a81767b9a7
Aaron Simpson <aasimpson34@googlemail.com>
2022-01-29 11:26:57 +0000

    #2 Fix merge issues

--- core/src/main/java/states/GameStateHandler.java
+++ core/src/main/java/states/GameStateHandler.java
@@ -6,24 +6,23 @@ import com.badlogic.gdx.graphics.GL20;
 import worldmapstate.WorldMapState;
 
 public class GameStateHandler {
-	GameState m_currentGameState;
 	
 	public GameStateHandler()
 	{
-		m_currentGameState = new WorldMapState();
 	}
 	
 	public void render() {
 		Gdx.gl.glClearColor(1, 1, 1, 1); 
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); 
 		
-		m_currentGameState.render();
-		//System.out.println("render");
+
+		m_currentGameStates.peek().render();
 		
 	}
 
 	public void update() {
 		m_currentGameState = m_currentGameState.update();
+			m_currentGameStates.pop();
+		}
 	}
-
 }
--- core/src/main/java/worldmapstate/WorldMapState.java
+++ core/src/main/java/worldmapstate/WorldMapState.java
@@ -14,6 +14,11 @@ public class WorldMapState implements GameState {
 		GameRenderer.getInstance();
 		GameCamera.getInstance();
 		loadingScreen = new LoadingScreen();
+		m_debugWorldChunk[0] = new WorldChunk(0, 0);
+		m_debugWorldChunk[1] = new WorldChunk(-1, 0);
+		m_debugWorldChunk[2] = new WorldChunk(-1, -1);
+		m_debugWorldChunk[3] = new WorldChunk(0, -1);
+		
 	}
 	
 	@Override
@@ -21,6 +26,9 @@ public class WorldMapState implements GameState {
 
 		GameObjectEntityHandler.getInstance().update();
 		loadingScreen.update();
+		for(int x = 0; x < 4 ;x++)
+			m_debugWorldChunk[x].update();
+		
 		return this;
 	}
 
@@ -28,6 +36,9 @@ public class WorldMapState implements GameState {
 	public void render() {
 		GameRenderer.getInstance().getBatch().setProjectionMatrix(GameCamera.getInstance().getProjection());
 		GameRenderer.getInstance().getBatch().begin();
+		
+		for(int x = 0; x < 4 ;x++)
+			m_debugWorldChunk[x].render();
 		
 		GameObjectEntityHandler.getInstance().render();
 		loadingScreen.render();
