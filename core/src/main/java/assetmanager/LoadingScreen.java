7f42707b23068eefed97ed3d07a48694e73e5b40
Aaron Simpson <aasimpson34@googlemail.com>
2022-01-29 10:25:10 +0000

    #16 Initial pass of a loading screen/asset management system
    Co-Authored-By: Cullen Willis <cullenwillis96@gmail.com>

---
+++ core/src/main/java/assetmanager/LoadingScreen.java
@@ -0,0 +1,58 @@
+/**
+ * Project Name: GGJ2022-core 
+ * Created On: 29 Jan 2022
+ * file: loadingscreen.java
+ * Purpose of class: 
+ *
+ * Written by @author
+ */
+package assetmanager;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+import renderer.GameRenderer;
+
+/**
+ * @author aasim
+ *
+ */
+public class LoadingScreen {
+	
+	BitmapFont temporaryBitmapFont; //TODO: remove me.
+	
+	AssetManager m_assetManager;
+	
+	float m_loadProgress = 0.0f;
+
+	public LoadingScreen(){
+		m_assetManager = new AssetManager();
+		temporaryBitmapFont = new BitmapFont();
+		
+		m_assetManager.load("world_atlas.atlas",TextureAtlas.class);
+
+	}
+	
+	public boolean update(){
+		m_loadProgress = m_assetManager.getProgress();
+
+		if(m_assetManager.update()){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void render(){
+		temporaryBitmapFont.draw(GameRenderer.getInstance().getBatch(),"Loading..", 0, 0 );
+	}
+	
+	public float getProgress() {
+		return m_loadProgress;
+	}
+	
+	public TextureAtlas getTextureAtlas(String string) {
+		return m_assetManager.get(string, TextureAtlas.class);
+	}
+}
