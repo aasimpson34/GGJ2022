042a9555abb566227494dc13837ee9785331f49b
Cullen Willis <cullenwillis96@gmail.com>
2022-01-30 00:18:30 +0000

    #17 Origin point from camera

--- core/src/main/java/userInterface/WorldEntityUI.java
+++ core/src/main/java/userInterface/WorldEntityUI.java
@@ -15,7 +15,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
+import camera.GameCamera;
 import renderer.GameRenderer;
 import townentity.TownEntity;
 
@@ -26,11 +28,16 @@ import townentity.TownEntity;
 public abstract class WorldEntityUI {
 
 	private SpriteBatch batch;
+	private boolean isShowing = false;
+	private Vector2 originPoint = new Vector2(0, 0);
 	
 	public abstract void update();
 	public abstract void render();
 	
-	public void renderWindow(int x, int y, int width, int height, int textureType)
+	public void setIsShowing(boolean x) { this.isShowing = x; }
+	public boolean getIsShowing() { return this.isShowing; }
+	
+	public void renderWindow(float x, float y, float width, float height, int textureType)
 	{
 		batch = GameRenderer.getInstance().getBatch();
 		
@@ -52,7 +59,7 @@ public abstract class WorldEntityUI {
 		batch.draw(background, x, y, width, height);
 	}
 	
-	public boolean renderButton(String button_label, int x, int y, int width, int height)
+	public boolean renderButton(String button_label, float x, float y, float width, float height)
 	{
 		batch = GameRenderer.getInstance().getBatch();
 		Texture button = new Texture(Gdx.files.internal("user_interface/debug_white.png"));
@@ -74,7 +81,7 @@ public abstract class WorldEntityUI {
 		return false;
 	}
 	
-	public void renderLabel(String label, int x, int y, int width, Color colour, int scale, int align)
+	public void renderLabel(String label, float x, float y, float width, Color colour, int scale, int align)
 	{
 		batch = GameRenderer.getInstance().getBatch();
 		BitmapFont font = new BitmapFont();
@@ -84,6 +91,12 @@ public abstract class WorldEntityUI {
 		font.draw(batch, label, x, y + lineHeight, width, align, false);
 	}
 	
+	public void setOriginPoints() {
+		GameCamera camera = GameCamera.getInstance();
+		Vector2 cameraPos = camera.getPosition();
+		this.originPoint = cameraPos;
+	}
+	public Vector2 getOrigin() { return this.originPoint; }
 	public TownEntity townEntity;
 	public void setTownEntity(TownEntity town) { this.townEntity = town; }
 	public TownEntity getTownEntity() { return this.townEntity; }
